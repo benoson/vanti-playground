a7599a63cb746ccd28f50811351b8ab027cb93e1
benoson <ben@Bens-MacBook-Pro.local>
2021-12-13 13:11:04 +0200

    Init

---
+++ src/PlaygroundContainer.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+const PlaygroundContainer = () => {
+  const [allStations, setNewStation] = useState([]);
+
+  return <div></div>;
+};
+
+export default PlaygroundContainer;
